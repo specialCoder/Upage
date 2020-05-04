24058a880aa8d734a8df372cb0d1530d22fe374d
zhubenlei <zhubenlei@bytedance.com>
2020-05-04 11:39:20 +0800

    添加todo-list

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Layout from '../../components/Layout/index.jsx';
 import Content from '../../components/Layout/Content.jsx';
 import TopBox from '../TopBox/index.jsx';
+import TodoList from '../TodoList/index.jsx';
 import './style.less';
 
 const Home  = ()=>{
@@ -9,6 +10,7 @@ const Home  = ()=>{
     <Layout/>
     <Content>
       <TopBox/>
+      <TodoList/>
     </Content>
   </div>;
 };
